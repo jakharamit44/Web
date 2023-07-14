c17d2dd779e38379b9f0d80593b017c190706182
jakharamit44 <125469531+jakharamit44@users.noreply.github.com>
2023-07-14 17:33:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 bot_token = os.environ.get("TOKEN", "5771926112:AAEBetnNf9hkNyUVCnHVMlgqMAsCItmYwdM")
 api_hash = os.environ.get("HASH", "ad762fe0516f367115ba651d929cf429") 
 api_id = os.environ.get("ID", "17737898")
+
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)  
 
 def fetch(url):
@@ -34,16 +35,18 @@ def fetch(url):
 def receive(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
     msg = message.reply_text("Fetching...")
     res = fetch(message.text)
-    final = []
-    tmp = ""
-    for ele in res:
-        tmp += ele + "\n\n"
-        if len(tmp) > 3950:
-            final.append(tmp)
-            tmp = ""
-    final.append(tmp)
+    # final = []
+    # tmp = ""
+    # for ele in res:
+    #     tmp += ele + "\n\n"
+    #     if len(tmp) > 3950:
+    #         final.append(tmp)
+    #         tmp = ""
+    # final.append(tmp)
+    print(len(res))
     msg.delete()
-    for msg in final:
-        message.reply_text(msg, disable_web_page_preview=True)
+    for ele in res:
+        message.reply_text(ele, disable_web_page_preview=True)
+        # time.sleep(1)
 
 app.run()
